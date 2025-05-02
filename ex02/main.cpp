61c2bb3ae64606db1d95eade69c3a3e255f55b0c
togauthi <togauthier23@gmail.com>
2025-05-02 10:05:49 +0200

    Removed comments

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,52 +1,6 @@
 #include"Fixed.hpp"
 #include <iostream>
 
-// int main( void ) {
-// 	Fixed a;
-// 	Fixed const b( 10 );
-// 	Fixed const c( 42.42f );
-// 	Fixed const d( b );
-// 	a = Fixed( 1234.4321f );
-// 	std::cout << "a is " << a << std::endl;
-// 	std::cout << "b is " << b << std::endl;
-// 	std::cout << "c is " << c << std::endl;
-// 	std::cout << "d is " << d << std::endl;
-// 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-// 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-// 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-// 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-// 	
-// 	std::cout << std::endl << "Custom tests :" << std::endl;
-//
-// 	std::cout << "a > b is " << (a > b) << std::endl;
-// 	std::cout << "a < b is " << (a < b) << std::endl;
-// 	std::cout << "a == b is " << (a == b) << std::endl;
-// 	std::cout << "a != b is " << (a != b) << std::endl;
-// 	std::cout << "a + b is " << (a + b) << std::endl;
-// 	std::cout << "a - b is " << (a - b) << std::endl;
-// 	std::cout << "a * b is " << (a * b) << std::endl;
-// 	std::cout << "a / b is " << (a / b) << std::endl;
-//
-// 	std::cout << std::endl << "Self increment/decrement:" << std::endl;
-//
-// 	std::cout << "++a " << ++a << std::endl;
-// 	std::cout << "After ++a " << a << std::endl;
-// 	std::cout << "a++ " << a++ << std::endl;
-// 	std::cout << "a++ " << a << std::endl;
-// 	std::cout << "--a " << --a << std::endl;
-// 	std::cout << "--a " << a << std::endl;
-// 	std::cout << "a-- " << a-- << std::endl;
-// 	std::cout << "a-- " << a << std::endl;
-//
-// 	std::cout << std::endl << "Static functions:" << std::endl;
-// 	std::cout << "max between a and b: " << Fixed::max(a, b) << std::endl;
-// 	std::cout << "min between a and b: " << Fixed::min(a, b) << std::endl;
-//
-//
-// 	return (0);
-// }
-//
-//
 int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
